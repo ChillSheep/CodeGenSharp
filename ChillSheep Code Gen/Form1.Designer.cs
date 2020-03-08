c353b1004752bf95acae36f7568d678603bce2ba
ChillSheep <52506697+ChillSheep@users.noreply.github.com>
2020-03-08 07:54:27 -0700

    Add files via upload

---
+++ ChillSheep Code Gen/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace CodeGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.discordToggle = new MetroFramework.Controls.MetroToggle();
+            this.discordLabel = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.customToggle = new MetroFramework.Controls.MetroToggle();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.saveCustomSettings = new MetroFramework.Controls.MetroButton();
+            this.metroProgressBar1 = new MetroFramework.Controls.MetroProgressBar();
+            this.metroTextBox3 = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(16, 309);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(675, 32);
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "Start";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(653, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.FontSize = MetroFramework.MetroTextBoxSize.Medium;
+            this.metroTextBox1.Lines = new string[] {
+        "How Many Codes To Generate?"};
+            this.metroTextBox1.Location = new System.Drawing.Point(15, 12);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(675, 23);
+            this.metroTextBox1.Style = MetroFramework.MetroColorStyle.Black;
+            this.metroTextBox1.TabIndex = 1;
+            this.metroTextBox1.Text = "How Many Codes To Generate?";
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.metroTextBox1.Click += new System.EventHandler(this.metroTextBox1_Click);
+            // 
+            // discordToggle
+            // 
+            this.discordToggle.AutoSize = true;
+            this.discordToggle.FontSize = MetroFramework.MetroLinkSize.Medium;
+            this.discordToggle.Location = new System.Drawing.Point(106, 41);
+            this.discordToggle.Name = "discordToggle";
+            this.discordToggle.Size = new System.Drawing.Size(80, 20);
+            this.discordToggle.Style = MetroFramework.MetroColorStyle.Purple;
+            this.discordToggle.TabIndex = 2;
+            this.discordToggle.Text = "Off";
+            this.discordToggle.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.discordToggle.UseSelectable = true;
+            this.discordToggle.CheckedChanged += new System.EventHandler(this.discordToggle_CheckedChanged);
+            // 
+            // discordLabel
+            // 
+            this.discordLabel.AutoSize = true;
+            this.discordLabel.Location = new System.Drawing.Point(16, 41);
+            this.discordLabel.Name = "discordLabel";
+            this.discordLabel.Size = new System.Drawing.Size(86, 20);
+            this.discordLabel.Style = MetroFramework.MetroColorStyle.Red;
+            this.discordLabel.TabIndex = 3;
+            this.discordLabel.Text = "Discord Gen";
+            this.discordLabel.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(16, 70);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(86, 20);
+            this.metroLabel1.Style = MetroFramework.MetroColorStyle.Red;
+            this.metroLabel1.TabIndex = 4;
+            this.metroLabel1.Text = "Custom Gen";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // customToggle
+            // 
+            this.customToggle.AutoSize = true;
+            this.customToggle.FontSize = MetroFramework.MetroLinkSize.Medium;
+            this.customToggle.Location = new System.Drawing.Point(106, 70);
+            this.customToggle.Name = "customToggle";
+            this.customToggle.Size = new System.Drawing.Size(80, 20);
+            this.customToggle.Style = MetroFramework.MetroColorStyle.Purple;
+            this.customToggle.TabIndex = 5;
+            this.customToggle.Text = "Off";
+            this.customToggle.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.customToggle.UseSelectable = true;
+            this.customToggle.CheckedChanged += new System.EventHandler(this.customToggle_CheckedChanged);
+            // 
+            // metroTextBox2
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox2.CustomButton.Image = null;
+            this.metroTextBox2.CustomButton.Location = new System.Drawing.Point(477, 1);
+            this.metroTextBox2.CustomButton.Name = "";
+            this.metroTextBox2.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox2.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox2.CustomButton.TabIndex = 1;
+            this.metroTextBox2.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox2.CustomButton.UseSelectable = true;
+            this.metroTextBox2.CustomButton.Visible = false;
+            this.metroTextBox2.Enabled = false;
+            this.metroTextBox2.Lines = new string[] {
+        "Enter The Custom String"};
+            this.metroTextBox2.Location = new System.Drawing.Point(192, 67);
+            this.metroTextBox2.MaxLength = 32767;
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.PasswordChar = '\0';
+            this.metroTextBox2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox2.SelectedText = "";
+            this.metroTextBox2.SelectionLength = 0;
+            this.metroTextBox2.SelectionStart = 0;
+            this.metroTextBox2.ShortcutsEnabled = true;
+            this.metroTextBox2.Size = new System.Drawing.Size(499, 23);
+            this.metroTextBox2.TabIndex = 6;
+            this.metroTextBox2.Text = "Enter The Custom String";
+            this.metroTextBox2.UseSelectable = true;
+            this.metroTextBox2.Visible = false;
+            this.metroTextBox2.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox2.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.CausesValidation = false;
+            this.metroLabel2.Enabled = false;
+            this.metroLabel2.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.metroLabel2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.metroLabel2.Location = new System.Drawing.Point(15, 254);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(606, 20);
+            this.metroLabel2.Style = MetroFramework.MetroColorStyle.Purple;
+            this.metroLabel2.TabIndex = 7;
+            this.metroLabel2.Text = "L for big letter, A for any character, N for number, l for small letter, a for an" +
+    "y letter, - for a -";
+            this.metroLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroLabel2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroLabel2.Visible = false;
+            // 
+            // saveCustomSettings
+            // 
+            this.saveCustomSettings.Enabled = false;
+            this.saveCustomSettings.Location = new System.Drawing.Point(558, 96);
+            this.saveCustomSettings.Name = "saveCustomSettings";
+            this.saveCustomSettings.Size = new System.Drawing.Size(133, 23);
+            this.saveCustomSettings.TabIndex = 8;
+            this.saveCustomSettings.Text = "Save Custom String";
+            this.saveCustomSettings.UseSelectable = true;
+            this.saveCustomSettings.Visible = false;
+            this.saveCustomSettings.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroProgressBar1
+            // 
+            this.metroProgressBar1.Location = new System.Drawing.Point(15, 280);
+            this.metroProgressBar1.Name = "metroProgressBar1";
+            this.metroProgressBar1.Size = new System.Drawing.Size(675, 23);
+            this.metroProgressBar1.Style = MetroFramework.MetroColorStyle.Purple;
+            this.metroProgressBar1.TabIndex = 9;
+            // 
+            // metroTextBox3
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox3.CustomButton.Image = null;
+            this.metroTextBox3.CustomButton.Location = new System.Drawing.Point(117, 2);
+            this.metroTextBox3.CustomButton.Name = "";
+            this.metroTextBox3.CustomButton.Size = new System.Drawing.Size(15, 15);
+            this.metroTextBox3.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox3.CustomButton.TabIndex = 1;
+            this.metroTextBox3.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox3.CustomButton.UseSelectable = true;
+            this.metroTextBox3.CustomButton.Visible = false;
+            this.metroTextBox3.Lines = new string[] {
+        "codes.txt"};
+            this.metroTextBox3.Location = new System.Drawing.Point(555, 41);
+            this.metroTextBox3.MaxLength = 32767;
+            this.metroTextBox3.Name = "metroTextBox3";
+            this.metroTextBox3.PasswordChar = '\0';
+            this.metroTextBox3.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox3.SelectedText = "";
+            this.metroTextBox3.SelectionLength = 0;
+            this.metroTextBox3.SelectionStart = 0;
+            this.metroTextBox3.ShortcutsEnabled = true;
+            this.metroTextBox3.Size = new System.Drawing.Size(135, 20);
+            this.metroTextBox3.TabIndex = 10;
+            this.metroTextBox3.Text = "codes.txt";
+            this.metroTextBox3.UseSelectable = true;
+            this.metroTextBox3.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox3.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.metroTextBox3.Click += new System.EventHandler(this.metroTextBox3_Click);
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.CausesValidation = false;
+            this.metroLabel3.Enabled = false;
+            this.metroLabel3.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.metroLabel3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.metroLabel3.Location = new System.Drawing.Point(414, 41);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(135, 20);
+            this.metroLabel3.Style = MetroFramework.MetroColorStyle.Purple;
+            this.metroLabel3.TabIndex = 11;
+            this.metroLabel3.Text = "Filename for codes";
+            this.metroLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroLabel3.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(702, 353);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroTextBox3);
+            this.Controls.Add(this.metroProgressBar1);
+            this.Controls.Add(this.saveCustomSettings);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroTextBox2);
+            this.Controls.Add(this.customToggle);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.discordLabel);
+            this.Controls.Add(this.discordToggle);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.metroButton1);
+            this.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(720, 400);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(720, 400);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroToggle discordToggle;
+        private MetroFramework.Controls.MetroLabel discordLabel;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroToggle customToggle;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroButton saveCustomSettings;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox3;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+    }
+}
+
---
+++ ChillSheep Code Gen/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CodeGen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ulong codesAmmount;
+        Random rnd = new Random();
+        char[] allAlphaNumberic = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".ToCharArray();
+        char[] anyLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".ToCharArray();
+        char[] bigLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        char[] smallLetters = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        char[] numbers = "0123456789".ToCharArray();
+        char[] discord = new char[16];
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.fileName == "")
+            {
+                MessageBox.Show("Filepath cannot be empty");
+                Application.Exit();
+            }
+            try 
+            {
+                StreamWriter writer3 = new StreamWriter(Properties.Settings.Default.fileName);
+                writer3.Close();
+                Properties.Settings.Default.fileName = metroTextBox3.Text;
+                Properties.Settings.Default.Save();
+            }
+            catch { MessageBox.Show("File " + Properties.Settings.Default.fileName + " is currently in use or name is illegal\nI have reset the filename for you :)");
+                Properties.Settings.Default.fileName = "codes.txt"; metroTextBox3.Text = Properties.Settings.Default.fileName;
+            }
+            
+            if (metroTextBox1.Text != "How Many Codes To Generate?")
+                try
+                {
+                    if (UInt64.Parse(metroTextBox1.Text) > 0)
+                    {
+                        codesAmmount = UInt64.Parse(metroTextBox1.Text);
+                        StreamWriter writer = new StreamWriter(Properties.Settings.Default.fileName);
+                    if (discordToggle.Checked == true)
+                    {
+                        for (ulong i = 0; i < codesAmmount; i++)
+                        {
+                            if (i%10==0)
+                                    if ((codesAmmount / 100) != 0)
+                                    {
+                                        metroProgressBar1.Value = (int)i / (int)(codesAmmount / 100);
+                                    }
+                            for (int j = 0; j < 16; j++)
+                                discord[j] = allAlphaNumberic[rnd.Next(0, 62)]; //allAlphaNumberic.Length
+                            writer.WriteLine("https://discordapp.com/gifts/" + new string(discord));
+                        }
+                            writer.Close();
+                            writer.Dispose();
+                            metroProgressBar1.Value = 100;
+                            MessageBox.Show("DONE!");
+                            metroProgressBar1.Value = 0;
+                    }
+                    else if (customToggle.Checked == true)
+                    {
+                        try
+                        {
+                            char[] custom = metroTextBox2.Text.ToCharArray();
+                            char[] now = new char[metroTextBox2.Text.Length];
+                            try
+                                {
+                                    for (uint i = 0; i < codesAmmount; i++)
+                                    {
+                                        if (i % 10 == 0)
+                                            if ((codesAmmount / 100) != 0)
+                                            {
+                                            metroProgressBar1.Value = (int)i / (int)(codesAmmount / 100);
+                                            }
+                                        for (int j = 0; j < metroTextBox2.Text.Length; j++)
+                                        {
+                                            if (custom[j] == 'A')
+                                                now[j] = allAlphaNumberic[rnd.Next(0, 62)];
+                                            else if (custom[j] == 'N')
+                                                now[j] = numbers[rnd.Next(0, 10)];
+                                            else if (custom[j] == 'L')
+                                                now[j] = bigLetters[rnd.Next(0, 26)];
+                                            else if (custom[j] == 'l')
+                                                now[j] = smallLetters[rnd.Next(0, 26)];
+                                            else if (custom[j] == 'a')
+                                                now[j] = anyLetter[rnd.Next(0, 52)];
+                                            else if (custom[j] == '-')
+                                                now[j] = '-';
+                                            else
+                                            {
+                                                throw new System.InvalidOperationException("Incorrect string from input, the program will now exit!");
+                                            }
+                                        }
+                                        writer.WriteLine(new string(now));
+                                    }
+                                }
+                                catch (Exception ef) { MessageBox.Show(ef.ToString()); MessageBox.Show("Incorrect string for custom gen, the program will now exit!"); Application.Exit(); }
+                                writer.Close();
+                                writer.Dispose();
+                                MessageBox.Show("DONE!");
+                        }
+                        catch { MessageBox.Show("Enter a smaller string in the custom box"); writer.Close();  }
+                    }
+                    else { MessageBox.Show("You need to enable a module!"); writer.Close(); }
+                    }
+                    else
+                        MessageBox.Show("Put a number bigger than 0");
+                }
+                catch { MessageBox.Show("Please put a number, smaller than 18,446,744,073,709,551,615"); }
+            else
+                MessageBox.Show("Please input a number");
+        }
+
+        private void metroTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void discordToggle_CheckedChanged(object sender, EventArgs e)
+        {
+            if (customToggle.Checked == true)
+            {
+                discordToggle.Checked = false;
+            }
+        }
+
+        private void customToggle_CheckedChanged(object sender, EventArgs e)
+        {
+            if (discordToggle.Checked == true || customToggle.Checked == false)
+            {
+                customToggle.Checked = false;
+                metroTextBox2.Enabled = false;
+                metroTextBox2.Visible = false;
+                metroLabel2.Enabled = false;
+                metroLabel2.Visible = false;
+                saveCustomSettings.Enabled = false;
+                saveCustomSettings.Visible = false;
+            }
+            else
+            {
+                if (Properties.Settings.Default.customString != "Enter The Custom String")
+                    metroTextBox2.Text = Properties.Settings.Default.customString;
+                metroTextBox2.Enabled = true;
+                metroLabel2.Enabled = true;
+                metroTextBox2.Visible = true;
+                metroLabel2.Visible = true;
+                saveCustomSettings.Enabled = true;
+                saveCustomSettings.Visible = true;
+            }
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.customString = metroTextBox2.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            metroTextBox3.Text = Properties.Settings.Default.fileName;
+        }
+
+        private void metroTextBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
